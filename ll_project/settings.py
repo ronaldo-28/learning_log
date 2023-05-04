dd7f2012710ebc26453ff28ffe9a08c3c5f1888b
Ronaldo Fernandes <54996426+ronaldo-28@users.noreply.github.com>
2023-05-04 11:59:51 +0530

    Update settings.py

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mibxt7sboxg%r)9t6z=a6@weyo(thgbknfcwgyowgx9$5s4&=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
