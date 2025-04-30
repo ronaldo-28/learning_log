19b1e6e5309ef9daf931814b303f6a67fe915f5d
ronaldo <mafronfernandes28@gmail.com>
2025-04-30 23:51:04 +0530

    Fix: Add 'app' variable to wsgi.py for Vercel compatibility

--- ll_project/wsgi.py
+++ ll_project/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'll_project.settings')
 
+# This is the standard Django line
 application = get_wsgi_application()
+
+# Add this line for Vercel compatibility
+app = application
