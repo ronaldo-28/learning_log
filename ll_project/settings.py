4a5581f78266209720d8c86c8092bc0328463c7e
ronaldo <mafronfernandes28@gmail.com>
2025-05-01 01:02:35 +0530

    Trigger Vercel rebuild with updated build config

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -137,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'learning_logs:index'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
 LOGIN_URL = 'accounts:login'
+# Trigger rebuild
 
 
 # Vercel specific settings / CSRF
