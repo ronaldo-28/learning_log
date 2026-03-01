b5e6939eae9528b2270a60fa7e6f534cf2425ebf
ronaldo <mafronfernandes28@gmail.com>
2026-03-01 11:59:37 +0530

    fix dotenv ImportError on Vercel

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -3,9 +3,11 @@
 import os
 from pathlib import Path
 import dj_database_url
-from dotenv import load_dotenv
-
-load_dotenv()  # Loads .env file for local development
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
