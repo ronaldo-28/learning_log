5a5ae769e5b567048d7d147897032c0404711084
ronaldo <mafronfernandes28@gmail.com>
2026-03-01 19:07:24 +0530

    mofified login

--- accounts/views.py
+++ accounts/views.py
@@ -6,18 +6,16 @@ from django.contrib.auth.forms import UserCreationForm
 def register(request):
     """Register a new user."""
     if request.method != 'POST':
-        # Display blank registration form.
         form = UserCreationForm()
     else:
-        # Process completed form.
         form = UserCreationForm(data=request.POST)
 
         if form.is_valid():
             new_user = form.save()
-            # Log the user in and then redirect to home page.
-            login(request, new_user)
+            # Specify the backend explicitly to avoid ambiguity
+            login(request, new_user,
+                  backend='django.contrib.auth.backends.ModelBackend')
             return redirect('learning_logs:index')
 
-    # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'registration/register.html', context)
