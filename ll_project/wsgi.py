596905038290752bb10156ca632e081689489ada
ronaldo <mafronfernandes28@gmail.com>
2026-03-04 20:53:20 +0530

    Upload static files directly to GitHub

--- ll_project/wsgi.py
+++ ll_project/wsgi.py
@@ -1,24 +1,7 @@
 import os
-import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'll_project.settings')
-
-# Setup Django
-django.setup()
-
-from django.core.management import call_command
-from django.contrib.sites.models import Site  # <-- Imported Site model
-
-try:
-    # 1. Run migrations
-    call_command('migrate', '--noinput')
-    
-    # 2. Automatically fix the Site ID 500 error on the live database!
-    Site.objects.get_or_create(id=1, defaults={'domain': 'learning-log8.vercel.app', 'name': 'Learning Log'})
-
-except Exception as e:
-    print(f"Startup error: {e}")
 
 application = get_wsgi_application()
 app = application
